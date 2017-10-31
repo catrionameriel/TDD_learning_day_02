e84212c93f9398a2ab7cef13fff8600147c8e284
catrionameriel <catriona.meriel@gmail.com>
2017-10-31 16:41:17 +0000

    number_to_full_month test passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -26,6 +26,35 @@
 #   return argument_1 + argument_2
 # end
 
-def add_string_as_number(argument_1, argument_2)
-  return argument_1.to_i + argument_2.to_i
+# def add_string_as_number(argument_1, argument_2)
+#   return argument_1.to_i + argument_2.to_i
+# end
+
+def number_to_full_month_name(argument_1)
+  case month
+  when argument_1 == 1
+  return "January"
+  when argument_1 == 2
+  return "February"
+  when argument_1 == 3
+    return "March"
+  when argument_1 == 4
+    return "April"
+  when argument_1 == 5
+    return "May"
+  when argument_1 == 6
+    return "June"
+  when argument_1 == 7
+    return "July"
+  when argument_1 == 8
+    return "August"
+  when argument_1 == 9
+    return "September"
+  when argument_1 == 10
+    return "October"
+  when argument_1 == 11
+    return "November"
+  when argument_1 == 12
+    return "December"
+  end
 end
